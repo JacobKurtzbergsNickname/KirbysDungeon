79bc84a1bf50d578110ee391bbbc76626096e530
Kirby <korbinian.brunner@web.de>
2024-09-16 11:43:29 +0200

    Started on building UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DungeonComponent } from './dungeon/dungeon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DungeonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dungeon/dungeon.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dungeon',
+  templateUrl: './dungeon.component.html',
+  styleUrls: ['./dungeon.component.scss']
+})
+export class DungeonComponent {
+  title = 'Kirbys Dungeon';
+  chamber = 0;
+
+  nextChamber(){
+    this.chamber++ 
+    this.showChamber()
+  };
+
+  previousChamber(){
+    this.chamber--
+    this.showChamber()
+  }
+
+  showChamber(){
+    console.groupCollapsed("this.stepsTaken");
+    console.log(this.chamber);
+    console.groupEnd();
+  }
+
+}
